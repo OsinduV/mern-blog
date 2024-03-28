c9952900f6f3807ab0032266cc58bfc7c6bf13c6
OsinduV <146355128+OsinduV@users.noreply.github.com>
2024-03-29 01:38:56 +0530

    add functionality to the sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,49 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const[loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e)=>{
+    setFormData({...formData, [e.target.id]: e.target.value.trim()}); // ...formData - keeps the previous values(spread operator)
+  };
+
+  const handleSubmit = async (e) => {
+    //prevent default behavior of a form - because when we submit the form normally the page going to be refresh
+   e.preventDefault();
+   if(!formData.username || !formData.email || !formData.password){
+    return setErrorMessage('Please fill out all fields.');
+   }
+   try {
+    setLoading(true);
+    setErrorMessage(null);//clean errors from previous requests
+    const res = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'}, //because the type of thigs we are sending is json
+      //because we cannot send the json completely, we need to convert it to string and then send it
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    if(data.success === false){
+      return setErrorMessage(data.message);
+    }
+    setLoading(false);
+    if(res.ok){
+      navigate('/sign-in');
+    }
+   } catch (error) {
+    setErrorMessage(error.message);
+    setLoading(false);
+   }
+   
+  };
+
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -23,21 +64,28 @@ export default function SignUp() {
 
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your user name" />
-              <TextInput type="text" placeholder="username" id="username" />
+              <TextInput type="text" placeholder="username" id="username" onChange={handleChange}/>
             </div>
             <div>
               <Label value="Your email" />
-              <TextInput type="text" placeholder="name@company.com" id="email" />
+              <TextInput type="email" placeholder="name@company.com" id="email" onChange={handleChange}/>
             </div>
             <div>
               <Label value="Your password" />
-              <TextInput type="text" placeholder="Password" id="password" />
+              <TextInput type="password" placeholder="Password" id="password" onChange={handleChange}/>
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {
+                loading ? (
+                  <>
+                  <Spinner size='sm'/>
+                  <span className="pl-3">Loading...</span>
+                  </>
+                ) : 'Sign Up'
+              }
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -46,6 +94,11 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
