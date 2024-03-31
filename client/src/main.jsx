352ca2f53b3ea6ebc9eefd3658461ceb4e672825
OsinduV <146355128+OsinduV@users.noreply.github.com>
2024-03-31 10:53:35 +0530

    Add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
 
 // we have to wrap our whole application with redux toolkit so the global state is going to be available in all application
