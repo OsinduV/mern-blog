51c1e6b282c57d1103681daa50d6449df3e13081
OsinduV <146355128+OsinduV@users.noreply.github.com>
2024-04-02 19:37:52 +0530

    complete sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
+
+export default function DashSidebar() {
+  const location = useLocation(); // to find out which tab we are in.
+  const [tab, setTab] = useState(""); //initial val - ''(empty string)
+  useEffect(
+    () => {
+      const urlParams = new URLSearchParams(location.search);
+      const tabFromUrl = urlParams.get("tab");
+      if (tabFromUrl) {
+        setTab(tabFromUrl);
+      }
+    },
+    [location.search] // any time location.search change, we want to render this use effect
+  );
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+            SignOut
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useLocation} from 'react-router-dom'; //
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 export default function Dashboard() {
+  const location = useLocation();// to find out which tab we are in.
+  const [tab, setTab] = useState('')//initial val - ''(empty string)
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab')
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  }, [location.search] // any time location.search change, we want to render this use effect
+  );
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className="md:w-56">
+      {/* sidebar */}
+      <DashSidebar/>
+      </div>
+      {/* profile..... */}
+      {tab === 'profile' && <DashProfile/>}
+
+    </div>
   )
 }
