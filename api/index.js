add0b52cd0dc3e626e00f82a922304c94552495a
OsinduV <146355128+OsinduV@users.noreply.github.com>
2024-04-05 00:39:12 +0530

    Add update user API route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
+import cookieParser from "cookie-parser"; // inorder to get cookie in the backend
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ mongoose
 const app = express();
 
 app.use(express.json());//allow json as the input of the backend
+app.use(cookieParser());//then we can extract cookie from browser without any problem
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
